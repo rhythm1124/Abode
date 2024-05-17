92e90664e3d679c4136f33a9d9454bc0c008fa00
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-17 10:58:14 +0530

    Create login_page.js

---
+++ src/components/login_page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './login_page.css';
+
+const login_page=()=>{
+    return(
+        <div className='login1'>
+            
+        </div>
+    );
+};
+
+export default login_page;

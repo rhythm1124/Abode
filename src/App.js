a665aa6128fa471b95bf7f99d1eb6cf51672c822
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-18 18:33:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import login_page from './components/login_page'
+import Main from './components/main.js';
+import Navbar from './components/navbar.js';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <login_page/>
+      <Main/>
     </div>
   );
 }

4fe5a3fabc8745f861fc067cce6ee8eb38de64f7
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-18 18:33:28 +0530

    Create main.js

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css';
+
+function Main () {
+    return (
+        <div className="main">
+            <div className="main_container"></div>
+        </div>
+    )
+}
+
+export default Main;

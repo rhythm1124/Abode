62c8d96777493816a369486e44aea62abdd24321
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-18 19:19:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import login_page from './components/login_page'
+<<<<<<< Updated upstream
 import Main from './components/main.js';
 import Navbar from './components/navbar.js';
+=======
+>>>>>>> Stashed changes
 
 function App() {
   return (

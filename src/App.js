729ad24012f62e11dcd6a4139cd8118dd64ef0e1
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-18 18:36:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import login_page from './components/login_page'
+import React from 'react';
+import LoginPage from './components/login_page';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <login_page/>
+      <LoginPage/>
     </div>
   );
 }
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* <Route path="/" element={<WelcomePage/>} /> */}
+//         <Route path="/login" element={<login_page/>}/>
+//       </Routes>
+//     </Router>
+//   );
+// };  
 
 export default App;

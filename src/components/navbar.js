5b0796777cbdbfc72a8e1c8fb3a8c7402f5781ee
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-18 18:33:24 +0530

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="leftside"></div>
+            <div className="rightside"></div>
+        </div>
+    );
+}
+
+export default Navbar;
+
+// left side: add logo which navigates to home
+// Right side: all the navigating link buttons

62b341419603d8dc2297f444ed45de58a8550ab2
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-20 01:24:25 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
 import Logo from '../assets/logo.png';
-import Cart from '../assets/cart.png'
+import Cart from '../assets/cart.png';
+import SearchIcon from '../assets/search.png';
 import './navbar.css';
 
 const Navbar = () => {
@@ -15,8 +16,7 @@ const Navbar = () => {
             <div className="rightside">
                 <ul>
                     <li>Categories</li>
-                    <li><input type="text" placeholder="Search" className="Search-bar" />
-                    <img className="Search-icon" alt="" src={SearchIcon} /></li>
+                    <li><input type="text" placeholder="Search" className="Search-bar" /></li>
                     <li><img className="Cart" src={Cart} alt=""/></li>
                 </ul>
             </div>

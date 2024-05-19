e8dedd3b2ba520e4e9d58e900e8b9002a90b281c
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-20 02:56:13 +0530

    Create Landing-page.js

---
+++ src/components/Landing-page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import './Landing-page.css';
+
+function Main () {
+    return (
+        <div className="main">
+            <div className="main_container">
+                <div className="content-container">
+                    <h1>explore</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;

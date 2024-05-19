b56031d52c62e963cc2bed99e5ae3e48785ae9ec
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-20 04:28:14 +0530

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -6,12 +6,16 @@ function Main () {
     return (
         <div className="main">
             <div className="main_container">
-                <div className="overlay">
                     <div className="content">
                         <h1>title</h1>
                         <p>and the journey begins</p>
                     </div>
-                </div>
+            </div>
+
+            <div classname="grid-containter">
+                <div className="grid-item">1</div>
+                <div className="grid-item">1</div>
+                <div className="grid-item">1</div>
             </div>
 
         </div>

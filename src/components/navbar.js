a14de7c734de62da3af3e8759d0454747e27008f
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-20 01:12:44 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
 import Logo from '../assets/logo.png';
+import Cart from '../assets/cart.png'
 import './navbar.css';
 
 const Navbar = () => {
@@ -8,13 +9,13 @@ const Navbar = () => {
         <div className="navbar">
             <div className="leftside">
             <div className="logo_container">
-                <img className="logo" src={Logo}/>
+                <img className="logo" src={Logo} alt=""/>
             </div>
             </div>
             <div className="rightside">
                 <ul>
                     <li>Categories</li>
-                    <li>cart</li>
+                    <li><img className="Cart" src={Cart} alt=""/></li>
                     <li>search</li>
                     <li>exit</li>
                 </ul>

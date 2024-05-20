f19b9abcde176a7b1a5943691e3ac084971565a8
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-21 04:56:03 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import WelcomePage from './components/welcomepage';
 import Main from './components/main.js';
 import Navbar from './components/navbar.js';
 import Collection from './components/Collection.js';
+import ContactUs from './components/ContactUs.js';
 function App() {
   return (
     <div>
       {/* <LoginPage/> */}
-      <SignupPage/>
+      {/* <SignupPage/> */}
       {/* <WelcomePage/> */}
+      <ContactUs/>
     </div>
   );
 }

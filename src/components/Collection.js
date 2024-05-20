c004ed5bad97dc998d943ed6ef5bcd40577fe8fe
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-20 16:00:09 +0530

    setting up collection

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Collection.css';
+import lamp from '../assets/lamp12.jpeg'
 
 const Collection = () => {
     return(
-        <div className="container">
-            <div className="Content">
+        <div className="collection-container">
+            <div className="content-container">
                 <h1>title</h1>
                 <p>summary types</p>
             </div>
-            <div className="collection-container">
-                <div className="collection-item"></div>
+            <div className="collection-grid-container">
+                <div className="collection-item">
+                    <img src={lamp} alt="lamp" className="collection-item-img"/>
+                    <h2>title</h2>
+                    <p>know more</p>
+                    <button className="item-increment">+</button>
+                    <input type="text" className="output-display"></input>
+                    <button className="item-decrement">-</button>
+                    <button className="item-delete">Del</button>
+                </div>
             </div>
         </div>
     );

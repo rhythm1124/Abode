40c923d03c652d48bf0cafc2e3a16d3cf6d77a1b
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-21 04:23:21 +0530

    main

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 // import LoginPage from './components/login_page';
 // import SignupPage from './components/signup_page';
 // import WelcomePage from './components/welcomepage';
-// import Main from './components/main.js';
+import Main from './components/main.js';
 import Navbar from './components/navbar.js';
-import Collection from './components/Collection.js';
+// import Collection from './components/Collection.js';
 // import CartDropdown from './components/CartDropdown.js';
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
       {/* <WelcomePage/> */}
       <Navbar />
       {/* <CartDropdown/> */}
-      {/* <Main/> */}
-      <Collection/>
+      <Main/>
+      {/* <Collection/> */}
       {/* <SignupPage/> */}
     </div>
   );

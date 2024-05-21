a9d227437bae30c386fb7f4f96c9459bf747711a
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-21 13:05:14 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import SignupPage from './components/signup_page';
 // import WelcomePage from './components/welcomepage';
 import Main from './components/main.js';
-import Navbar from './components/navbar.js';
+// import Navbar from './components/navbar.js';
 import ContactUs from './components/ContactUs.js';
 // import Collection from './components/Collection.js';
 // import CartDropdown from './components/CartDropdown.js';

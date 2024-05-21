c93cd53fbc4a51c46cb9f5389787d384f696f36d
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-21 14:25:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 // import LoginPage from './components/login_page';
 // import SignupPage from './components/signup_page';
 // import WelcomePage from './components/welcomepage';
-import Main from './components/main.js';
-import Navbar from './components/navbar.js';
-import ContactUs from './components/ContactUs.js';
+// import Main from './components/main.js';
+// import ContactUs from './components/ContactUs.js';
 // import Collection from './components/Collection.js';
 // import CartDropdown from './components/CartDropdown.js';
 

4895d7e9087f5903f6d399933965a02814552fa3
r1ya48 <122423136+r1ya48@users.noreply.github.com>
2024-05-21 16:08:25 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import './App.css';
 import LoginPage from './components/login_page';
-// import SignUp from './components/signup_page';
-// import WelcomePage from './components/welcomepage.js';
-// import Main from './components/main.js';
-// import ContactUs from './components/ContactUs.js';
+import SignUp from './components/signup_page';
+import WelcomePage from './components/welcomepage.js';
+import Main from './components/main.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactUs from './components/ContactUs.js';
 import Collection from './components/Collection.js';
-// import CartDropdown from './components/CartDropdown.js';
+import CartDropdown from './components/CartDropdown.js';
 import PhoneSignup from './components/phone_signup.js';
 
-function App() {
-  return (
-    <div>
-      {/* <LoginPage/> */}
-      {/* <SignUp/> */}
-      {/* <WelcomePage/> */}
-      {/* <CartDropdown/> */}
-      {/* <Main/> */}
-      <Collection/>
-      {/* <ContactUs/> */}
-      <PhoneSignup/>
-    </div>
-  );
-}
-// const App = () => {
+// function App() {
 //   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<WelcomePage/>} />
-//         <Route path="/login" element={<LoginPage/>}/>
-//         <Route path="/SignUp" element={<SignUp/>}/>
-//         <Route path="/PhoneSignup" element={<PhoneSignup/>}/>
-//         <Route path="/Main" element={<Main/>}/>
-//         <Route path="/Collection" element={<Collection/>}/>
-//         <Route path="/Cart" element={<CartDropdown/>}/>
-//         <Route path="/ContactUs" element={<ContactUs/>}/>
-//       </Routes>
-//     </Router>
+//     <div>
+//       {/* <LoginPage/> */}
+//       {/* <SignUp/> */}
+//       {/* <WelcomePage/> */}
+//       {/* <CartDropdown/> */}
+//       {/* <Main/> */}
+//       <Collection/>
+//       {/* <ContactUs/> */}
+//       <PhoneSignup/>
+//     </div>
 //   );
-// };  
+// }
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage/>} />
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/SignUp" element={<SignUp/>}/>
+        <Route path="/PhoneSignup" element={<PhoneSignup/>}/>
+        <Route path="/Main" element={<Main/>}/>
+        <Route path="/Collection" element={<Collection/>}/>
+        <Route path="/Cart" element={<CartDropdown/>}/>
+        <Route path="/ContactUs" element={<ContactUs/>}/>
+      </Routes>
+    </Router>
+  );
+};  
 
 export default App;
--- src/components/login_page.js
+++ src/components/login_page.js
@@ -1,109 +1,103 @@
 import React from 'react';
 import './login_page.css';
 import backgroundImage from './bg image3.jpg';
-// import { auth } from '../firebase'
-// import {useNavigate} 'react-router-dom';
-// const provider = new GoogleAuthProvider();
+import { auth } from '../firebase'
+import {useNavigate} from 'react-router-dom';
+const provider = new GoogleAuthProvider();
 
 const login_page=()=>{
-    // const [username, setUserName] = useState("");
-    // const [password, setPassword] = useState("");
-    // const navigate = useNavigate();
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    // const googleLogIn = (e) => {
-    //     e.preventDefault();
-    //     signInWithPopup(auth, provider)
-    //         .then((result) => {
-    //             // This gives you a Google Access Token. You can use it to access the Google API.
-    //             const credential = GoogleAuthProvider.credentialFromResult(result);
-    //             const token = credential.accessToken;
-    //             // The signed-in user info.
-    //             navigate('/welcomepg')
-    //             const user = result.user;
-    //             console.log(user)
-    //             console.log("Success")
-    //             // IdP data available using getAdditionalUserInfo(result)
-    //             // ...
-    //         }).catch((error) => {
-    //             // Handle Errors here.
-    //             const errorCode = error.code;
-    //             const errorMessage = error.message;
-    //             // The email of the user's account used.
-    //             const email = error.customData.email;
-    //             // The AuthCredential type that was used.
-    //             const credential = GoogleAuthProvider.credentialFromError(error);
-    //             // ...
-    //         });
-    // }
+    const googleLogIn = (e) => {
+        e.preventDefault();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                navigate('/welcomepg')
+                const user = result.user;
+                console.log(user)
+                console.log("Success")
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
 
-    // const handleUsernameChange = (e) => {
-    //     setUserName(e.target.value);
-    // };
+    const handleUsernameChange = (e) => {
+        setUserName(e.target.value);
+    };
 
-    // const handlePasswordChange = (e) => {
-    //     setPassword(e.target.value);
-    // }
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
 
-    // const handleSignUpClick = () => {
-    //     navigate('/signup');
-    //   };
+    const handleSignUpClick = () => {
+        navigate('/signup');
+      };
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     setTimeout(() => {
-    //         navigate('/signup');
-    //     },1000);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTimeout(() => {
+            navigate('/signup');
+        },1000);
         
-    // };
-    // const phoneClick = () => {
-    //     navigate('/phone_sign_in');
-    // }
-    // const onLogin = (e) => {
-    //     console.log("Signing in: " + username + ", " + password);
-    //     e.preventDefault();
-    //     signInWithEmailAndPassword(auth, username, password)
-    //     .then((userCredential) => {
-    //         // Signed in
-    //         const user = userCredential.user;
-    //         navigate('/welcomepg')
-    //         console.log(user);
-    //         console.log("success")
-    //     })
-    //     .catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         console.log(errorCode, errorMessage)
-    //     });
+    };
+    const phoneClick = () => {
+        navigate('/phone_sign_in');
+    }
+    const onLogin = (e) => {
+        console.log("Signing in: " + username + ", " + password);
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            navigate('/welcomepg')
+            console.log(user);
+            console.log("success")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+        });
        
-    // }
+    }
     return(
         <div className='outer1'>
         <div className='login'>
         <h1>Login</h1>
         <div className='abc'>
-        <button className="btn" >
-        {/* onClick={googleLogIn} */}
+        <button className="btn" onClick={googleLogIn}>
             <h3>Continue With Google</h3></button>
-        <button  className='btn1' >
-        {/* onClick={phoneClick} */}
+        <button  className='btn1' onClick={phoneClick}>
             <h3>Continue With PhoneNo.</h3>
         </button>
         <hr className='line'></hr>
         </div>
         <form>
             <div className='input-box'>
-                <input type='text' placeholder='Username*' />
-                {/* onClick={handleUsernameChange}required */}
+                <input type='text' placeholder='Username*' onClick={handleUsernameChange}required/>
             </div>
             <div className='input-box1'>
-                <input type='password' placeholder='Password*' />
-                {/* onChange={handlePasswordChange} required */}
+                <input type='password' placeholder='Password*' onChange={handlePasswordChange} required/>
             </div>
-                <center><button className='submit' >Login</button></center>
-                {/* onClick={onLogin} */}
+                <center><button className='submit' onClick={onLogin}>Login</button></center>
             <div className='register-link'>
-                <p>Don't have an account?<a href='#' >SignUp</a></p>
-                {/* onClick={handleSignUpClick} */}
+                <p>Don't have an account?<a href='#' onClick={handleSignUpClick}>SignUp</a></p>
             </div>
         </form>
     </div>

38187e3bb1e7134968fae7e31e2c631b9feda89c
rhythm1124 <122474433+rhythm1124@users.noreply.github.com>
2024-05-21 14:29:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import LoginPage from './components/login_page';
-// import SignupPage from './components/signup_page';
-// import WelcomePage from './components/welcomepage';
-import Main from './components/main.js';
-import Navbar from './components/navbar.js';
-import ContactUs from './components/ContactUs.js';
+// import LoginPage from './components/login_page';
+// import SignUp from './components/signup_page';
+import WelcomePage from './components/welcomepage';
+// import Main from './components/main.js';
+// import ContactUs from './components/ContactUs.js';
 // import Collection from './components/Collection.js';
 // import CartDropdown from './components/CartDropdown.js';
-import PhoneSignup from './components/phone_signup.js';
+// import PhoneSignup from './components/phone_signup.js';
 
 function App() {
   return (
     <div>
       {/* <LoginPage/> */}
       {/* <SignUp/> */}
-      {/* <WelcomePage/> */}
+      <WelcomePage/>
       {/* <CartDropdown/> */}
       {/* <Main/> */}
       {/* <Collection/> */}
       {/* <ContactUs/> */}
-      <PhoneSignup/>
+      {/* <PhoneSignup/> */}
     </div>
   );
 }
